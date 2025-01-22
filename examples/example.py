132f4fc137beb022599bbb6340b97862801bdd20
Gennaro <gennarinoos@gmail.com>
2025-01-21 17:54:49 -0800

    Test with simpler objects

--- examples/example.py
+++ examples/example.py
@@ -3,10 +3,35 @@
 
 import os
 
-from trimesh.base import Scene, Trimesh
+import trimesh
+from trimesh import Scene, Trimesh
 
 from neural_mesh_simplification import NeuralMeshSimplifier
 from neural_mesh_simplification.data.dataset import load_mesh
+from tests.test_metrics import create_cube_mesh
+
+
+def save_simplified_mesh(mesh: trimesh.Geometry, file_name: str):
+    """
+    Save the simplified mesh to file in the simplified folder.
+    """
+    simplified_dir = os.path.join(data_dir, "simplified")
+    if not os.path.exists(simplified_dir):
+        os.makedirs(simplified_dir)
+    output_path = os.path.join(simplified_dir, f"simplified_{file_name}")
+    mesh.export(output_path)
+
+    print(f"Simplified mesh saved to: {output_path}")
+
+
+def cubeExample():
+    file = "cube.obj"
+    mesh = create_cube_mesh()
+    output_path = os.path.join(data_dir, file)
+    mesh.export(output_path)
+    simplified_mesh = simplifier.simplify(mesh)
+    save_simplified_mesh(simplified_mesh, file)
+
 
 # Initialize the simplifier
 simplifier = NeuralMeshSimplifier()
@@ -23,32 +48,30 @@ for file_name in mesh_files:
     original_mesh = load_mesh(mesh_path)
 
     print("Loaded mesh at file" + mesh_path)
-    print(original_mesh)
 
     # Create a new scene to hold the simplified meshes
     simplified_scene = Scene()
 
     if isinstance(original_mesh, Trimesh):
+        print("Original: ", original_mesh)
         simplified_geom = simplifier.simplify(original_mesh)
-        print(simplified_geom)
+        print("Simplified: ", simplified_geom)
 
         simplified_scene = simplified_geom
 
     elif isinstance(original_mesh, Scene):
+        continue
+        
         # Iterate through the original mesh geometry
         for name, geom in original_mesh.geometry.items():
+            print("Original: ", geom)
             # Simplify each Trimesh object
             simplified_geom = simplifier.simplify(geom)
+            print("Simplified: ", simplified_geom)
             # Add the simplified geometry to the new scene
             simplified_scene.add_geometry(simplified_geom, geom_name=name)
     else:
         raise ValueError("Invalid mesh type (expected Trimesh or Scene):", type(original_mesh))
 
-    # Save the simplified scene
-    simplified_dir = os.path.join(data_dir, "simplified")
-    if not os.path.exists(simplified_dir):
-        os.makedirs(simplified_dir)
-    output_path = os.path.join(simplified_dir, f"simplified_{file_name}")
-    simplified_scene.export(output_path)
-
-    print(f"Simplified mesh saved to: {output_path}")
+    # Save the simplified mesh to file
+    save_simplified_mesh(simplified_scene, file_name)
